42ac883492b2af653c5fee0e9fc3e0e36a3e7e3d
someguy235 <someguy235@gmail.com>
2014-01-20 21:28:03 -0500

    updates

--- resources/js/controllers.js
+++ resources/js/controllers.js
@@ -4,3 +4,37 @@ var StudyHallModule = angular.module('StudyHallApp', []);
 $('.dropdown-menu').find('form').click(function (e) {
   e.stopPropagation();
 });
+  
+$('.btn').button()
+
+var StudyHallModule = angular.module('StudyHallApp', []);
+
+StudyHallModule.factory('StudyHallService', function(){
+  var service = {
+  }
+  return service
+})
+
+StudyHallModule.controller("LoginController", function($scope, $http, StudyHallService){
+
+});
+
+StudyHallModule.controller("FilterController", function($scope, $http, StudyHallService){
+
+});
+
+StudyHallModule.controller("MapController", function($scope, $http, StudyHallService){
+  $scope.map = L.map('map',
+    {
+      center: new L.LatLng(35.595, -82.552),
+      zoom: 12
+    }
+  );
+
+  var tileLayer = L.tileLayer(
+    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    { attribution: 'Map data © OpenStreetMap contributors' }
+  );
+
+  $scope.map.addLayer(tileLayer);
+});
